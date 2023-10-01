c88c13fcc0bdfba415b2dfc7d216565ea5c0fbd9
Mateusz Polito <matipolit@gmail.com>
2023-10-01 11:35:03 +0200

    ui fixes

--- src/pages/Vote.jsx
+++ src/pages/Vote.jsx
@@ -1,67 +1,97 @@
-import { Box, Center, Text, Button, Flex} from "@chakra-ui/react"
-import { useState } from "react"
-import VoteChoice from "./VoteChoice"
+import { Box, Center, Text, Button, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import VoteChoice from "./VoteChoice";
 
+function Vote() {
+    const [voting, setVoting] = useState(0);
 
+    return voting == 1 ? (
+        <VoteChoice />
+    ) : (
+        <Flex direction="column" gap="4" alignItems="center">
+            <Text fontSize="27px" color="#193769;" fontWeight="bold">
+                Upcoming elections
+            </Text>
+            <Text fontSize="23px" color="#193769;">
+                Next election is: <b>now</b>
+            </Text>
 
-function Vote(){
-const [voting, setVoting] = useState(0);
+            <Flex alignItems="center" alignContent="stretch" gap="4">
+                <Box
+                    border="2px"
+                    borderRadius="10px"
+                    borderColor="#193769;"
+                    display="flex"
+                    gap="4"
+                    alignItems="center"
+                    padding="2"
+                >
+                    <Text fontSize="40px" color="yellow">
+                        •
+                    </Text>
+                    <Text fontSize="20px" color="#193769;">
+                        &nbsp;&nbsp;EU Parliament 2024
+                    </Text>
+                </Box>
+                <Button
+                    backgroundColor="brand.500"
+                    color="white"
+                    onClick={() => setVoting(1)}
+                >
+                    Vote now
+                </Button>
+            </Flex>
 
-  return(voting == 1 ? <VoteChoice/> :
-
-
-    <Box padding="3">
-    <Text fontSize="27px" color="#193769;" fontWeight="bold">
-      Upcoming elections
-    </Text>
-    <Text fontSize="23px" color="#193769;">
-      Next election is in: 
-      <br></br>
-      123 days
-    </Text>
-    <Button backgroundColor="white" border="2px" borderColor="#193769;" margin="13px" onClick={() => setVoting(1)}>
-      <Text fontSize="40px" color="yellow">•</Text>
-      <Text fontSize="20px" color="#193769;">
-      &nbsp;&nbsp;EU Parliament 2024 
-      </Text>
-      <Text fontSize="23px" color="#193769;">
-        Next election is in: 
-        <br></br>
-        123 days
-      </Text>
-      <Button backgroundColor="white" border="2px" borderColor="#193769;" margin="13px" textAlign="center">
-        <Text fontSize="40px" color="yellow">•</Text>
-        <Text fontSize="20px" color="#193769;">
-        &nbsp;&nbsp;EU Parliament 2024 
-        </Text>
-      </Button>
-      <Text fontSize="23px" color="#193769;">
-        Your votes in previous election:
-      </Text>
-      <Button backgroundColor="white" border="2px" borderColor="#193769;" margin="13px" textAlign="center">
-        <Text fontSize="40px" color="red">•</Text>
-        <Flex direction="column">
-          <Text fontSize="20px" color="#193769;">
-          &nbsp;&nbsp;EU Parliament 2019
-          </Text>
-          <Text fontSize="10px" color="#193769;">
-          You didn't vote
-          </Text>
-        </Flex>  
-      </Button>
-      <br></br>
-      <Button backgroundColor="white" border="2px" borderColor="#193769;" margin="13px" textAlign="center">
-        <Text fontSize="40px" color="green">•</Text>
-        <Flex direction="column">
-          <Text fontSize="20px" color="#193769;">
-          &nbsp;&nbsp;EU Parliament 2014
-          </Text>
-          <Text fontSize="10px" color="#193769;">
-          &nbsp;&nbsp;You voted for:  Samuel Wojciechowski
-          </Text>
+            <Text fontSize="23px" color="#193769;">
+                Your votes in previous election:
+            </Text>
+            <Flex direction="column" gap="2">
+                <Flex
+                    backgroundColor="white"
+                    border="2px"
+                    borderColor="#193769;"
+                    textAlign="center"
+                    padding="2"
+                    borderRadius="10px"
+                    alignContent="center"
+                    alignItems="center"
+                >
+                    <Text fontSize="40px" color="red">
+                        •
+                    </Text>
+                    <Flex direction="column">
+                        <Text fontSize="20px" color="#193769;">
+                            &nbsp;&nbsp;EU Parliament 2019
+                        </Text>
+                        <Text fontSize="10px" color="#193769;">
+                            You didn't vote
+                        </Text>
+                    </Flex>
+                </Flex>
+                <Flex
+                    backgroundColor="white"
+                    border="2px"
+                    borderColor="#193769;"
+                    textAlign="center"
+                    padding="2"
+                    borderRadius="10px"
+                    alignContent="center"
+                    alignItems="center"
+                >
+                    <Text fontSize="40px" color="green">
+                        •
+                    </Text>
+                    <Flex direction="column">
+                        <Text fontSize="20px" color="#193769;">
+                            &nbsp;&nbsp;EU Parliament 2014
+                        </Text>
+                        <Text fontSize="10px" color="#193769;">
+                            &nbsp;&nbsp;You voted for: Samuel Wojciechowski
+                        </Text>
+                    </Flex>
+                </Flex>
+            </Flex>
         </Flex>
-      </Button>
-    </Flex>
-  )
+    );
 }
-export default Vote
+export default Vote;
