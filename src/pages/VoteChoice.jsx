a4216662949fec39fce872c0a41b5f0672316a28
emo-mruczek <krokcia1@gmail.com>
2023-10-01 10:11:02 +0200

    fix: voting

--- src/pages/VoteChoice.jsx
+++ src/pages/VoteChoice.jsx
@@ -11,6 +11,34 @@ return (
   src='https://bit.ly/dan-abramov'
   alt='Dan Abramov'
 /></Center></Box>
+<Box padding="3" borderRadius="2xl"  border="2px" borderColor="#193769;"><Center>
+<Flex direction='column' padding='3'><Text color='#193769;' as='b'>BERKOWICZ Konrad Szczepan</Text><Text>KOMITET WYBORCZY WYBORCÓW KONFEDERACJA KORWIN BRAUN LIROY NARODOWCY</Text></Flex><Image
+  borderRadius='full'
+  boxSize='100px'
+  src='https://bit.ly/dan-abramov'
+  alt='Dan Abramov'
+/></Center></Box>
+<Box padding="3" borderRadius="2xl"  border="2px" borderColor="#193769;"><Center>
+<Flex direction='column' padding='3'><Text color='#193769;' as='b'>BERKOWICZ Konrad Szczepan</Text><Text>KOMITET WYBORCZY WYBORCÓW KONFEDERACJA KORWIN BRAUN LIROY NARODOWCY</Text></Flex><Image
+  borderRadius='full'
+  boxSize='100px'
+  src='https://bit.ly/dan-abramov'
+  alt='Dan Abramov'
+/></Center></Box>
+<Box padding="3" borderRadius="2xl"  border="2px" borderColor="#193769;"><Center>
+<Flex direction='column' padding='3'><Text color='#193769;' as='b'>BERKOWICZ Konrad Szczepan</Text><Text>KOMITET WYBORCZY WYBORCÓW KONFEDERACJA KORWIN BRAUN LIROY NARODOWCY</Text></Flex><Image
+  borderRadius='full'
+  boxSize='100px'
+  src='https://bit.ly/dan-abramov'
+  alt='Dan Abramov'
+/></Center></Box>
+<Box padding="3" borderRadius="2xl"  border="2px" borderColor="#193769;"><Center>
+<Flex direction='column' padding='3'><Text color='#193769;' as='b'>BERKOWICZ Konrad Szczepan</Text><Text>KOMITET WYBORCZY WYBORCÓW KONFEDERACJA KORWIN BRAUN LIROY NARODOWCY</Text></Flex><Image
+  borderRadius='full'
+  boxSize='100px'
+  src='https://bit.ly/dan-abramov'
+  alt='Dan Abramov'
+/></Center></Box>
     </Flex>
 )   
 }
