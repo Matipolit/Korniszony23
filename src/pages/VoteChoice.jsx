d98930371dd311de747b9e648433d384ee11062f
emo-mruczek <krokcia1@gmail.com>
2023-10-01 10:09:49 +0200

    fix: voting

--- src/pages/VoteChoice.jsx
+++ src/pages/VoteChoice.jsx
@@ -1,13 +1,18 @@
-import { Flex, Box, Text, Image} from "@chakra-ui/react"
+import { Flex, Box, Text, Image, Center} from "@chakra-ui/react"
 
 
 function VoteChoice() {
 return (
-    <Flex>
-        <Box padding="3" borderRadius="2xl"  border="2px" borderColor="#193769;"><Image  borderRadius='full'
-  boxSize='150px' src='https://bit.ly/dan-abramov'></Image><Text color='#193769;'>BERKOWICZ Konrad Szczepan</Text><Text>KOMITET WYBORCZY WYBORCÓW KONFEDERACJA KORWIN BRAUN LIROY NARODOWCY</Text></Box>
+    <Flex direction='column' gap='5'>
+    <Box padding="3" borderRadius="2xl"  border="2px" borderColor="#193769;"><Center>
+<Flex direction='column' padding='3'><Text color='#193769;' as='b'>BERKOWICZ Konrad Szczepan</Text><Text>KOMITET WYBORCZY WYBORCÓW KONFEDERACJA KORWIN BRAUN LIROY NARODOWCY</Text></Flex><Image
+  borderRadius='full'
+  boxSize='100px'
+  src='https://bit.ly/dan-abramov'
+  alt='Dan Abramov'
+/></Center></Box>
     </Flex>
-)
+)   
 }
 
 export default VoteChoice;
