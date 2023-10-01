af28b86ce6eabf02a8f0c1ba9a679d84ad6394c6
M1K45 <272570@student.pwr.edu.pl>
2023-10-01 10:56:40 +0200

    Social
    Added politicians' posting feature.

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -15,6 +15,7 @@ import Home from "./pages/Home";
 import Latarnik from "./pages/Latarnik";
 import Candidates from "./pages/Candidates";
 import Vote from "./pages/Vote";
+import Social from "./pages/Social";
 
 const titles = ["Friends", "Profile", "Home", "Voting", "Meet the candidates"]
 
@@ -31,7 +32,7 @@ function Layout(){
   return(
     <Box>
       <Text fontSize="xx-large" fontWeight="bold">{title}</Text>
-      {index == 2 ? <Home /> : index == 3 ? <Vote /> : index == 4 ? <Candidates /> : <></>}
+      {index == 0 ? <Social /> : index == 2 ? <Home /> : index == 3 ? <Vote /> : index == 4 ? <Candidates /> : <></>}
       <BottomNavigation
         value={index}
         onChange={(newIndex) => {
