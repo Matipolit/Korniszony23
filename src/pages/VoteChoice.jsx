9721a14d16418060374b392dc888bfd92e18c4b3
emo-mruczek <krokcia1@gmail.com>
2023-10-01 09:49:35 +0200

    fix: voting

--- src/pages/VoteChoice.jsx
+++ src/pages/VoteChoice.jsx
@@ -1,7 +1,12 @@
+import { Flex, Box, Text, Image} from "@chakra-ui/react"
+
 
 function VoteChoice() {
 return (
-    <>xdxdxdxd</>
+    <Flex>
+        <Box padding="3" borderRadius="2xl"  border="2px" borderColor="#193769;"><Image  borderRadius='full'
+  boxSize='150px' src='https://bit.ly/dan-abramov'></Image><Text color='#193769;'>BERKOWICZ Konrad Szczepan</Text><Text>KOMITET WYBORCZY WYBORCÓW KONFEDERACJA KORWIN BRAUN LIROY NARODOWCY</Text></Box>
+    </Flex>
 )
 }
 
