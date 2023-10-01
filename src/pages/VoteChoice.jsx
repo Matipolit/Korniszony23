105802c1f0aba0b2259816f45d3e46f21ab6aa5a
emo-mruczek <krokcia1@gmail.com>
2023-10-01 09:37:29 +0200

    Create VoteChoice.jsx

---
+++ src/pages/VoteChoice.jsx
@@ -0,0 +1,15 @@
+import {Flex, Box, Text} from "@chakra-ui/react"
+
+function VoteChoice(){
+    return(
+        <Flex>
+<Box border="2px" borderColor="#193769;"  borderRadius="2xl">
+    <Text color='#193769;'>BERKOWICZ Konrad Szczepan</Text>
+    <Text>KOMITET WYBORCZY WYBORCÓW KONFEDERACJA KORWIN BRAUN LIROY NARODOWCY</Text>
+</Box>
+
+        </Flex>
+    )
+}
+
+export default VoteChoice;
